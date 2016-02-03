9803303ffa1373ee72e5be4e2a3d45b6e2386001
youm <youmblack@163.com>
2016-02-03 20:48:15 +0800

    add src/main.c include/std.h

---
+++ include/std.h
@@ -0,0 +1,33 @@
+/*
+*******************************************************************
+*this is new project Head
+*
+*
+*******************************************************************
+*/
+
+#ifndef _STD_H
+#define _STD_H
+
+typedef signed char		S8;
+typedef unsigned char	        U8;
+typedef signed short int   	S16;
+typedef unsigned short int	U16;
+typedef signed int		S32;
+typedef unsigned int		U32;
+typedef signed long long int    S64;
+typedef unsigned long long int  U64;
+typedef float			FP32;
+typedef double			FP64;
+
+#define BOOL    U8
+#define TURE    1
+#define FLASE   0
+#define ON      1
+#define OFF     0
+
+#ifndef NULL
+#define NULL    ((void*)0)
+#endif
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+***********************************************************
+*new project
+*
+*
+***********************************************************
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/std.h"
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
